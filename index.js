8c1d91a6ea340bcb6bc1009a2d6772246b125127
Brandt <b.peters132@gmail.com>
2021-11-29 19:10:17 -0600

    added basic stat tracking

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-import { startServer } from "./server.js"
-import { Client, Intents } from "discord.js"
-import fs from 'fs'
-import https from 'https'
-import dotenv from "dotenv"
-dotenv.config()
+import { startServer } from './server.js';
+import { Client, Intents } from 'discord.js';
+import fs from 'fs';
+import https from 'https';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILD_MEMBERS,
-  ],
-  disableMentions: 'everyone'
+    intents: [
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILDS
+    ],
+    disableMentions: 'everyone'
 });
-const app = startServer(client)
+const app = startServer(client);
 
 client.once('ready', async () => {
-  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-  client.user.setActivity('Serving the People');
-})
+    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+    var guildCount = await client.guilds.fetch();
+    guildCount = guildCount.size;
+    client.user.setActivity(`for raids on ${guildCount} servers`, { type: 'WATCHING' });
+    setInterval(client => {
+        client.user.setActivity(`for raids on ${guildCount} servers`, { type: 'WATCHING' });
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+    }, 21600 * 1000);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
 
 // Start Web/API server
 if (process.env.PRIVKEY && process.env.CERT) {
-  https.createServer({
-    key: fs.readFileSync(process.env.PRIVKEY),
-    cert: fs.readFileSync(process.env.CERT)
-  }, app).listen(process.env.PORT || 443, () => {
-    console.log('Web/API Server is running on port', process.env.PORT || 443);
-  });
+    https.createServer({
+        key: fs.readFileSync(process.env.PRIVKEY),
+        cert: fs.readFileSync(process.env.CERT)
+    }, app).listen(process.env.PORT || 443, () => {
+        console.log('Web/API Server is running on port', process.env.PORT || 443);
+    });
 } else {
-  app.listen(process.env.PORT || 8080, () => {
-    console.log('Web/API Server is running on port', process.env.PORT || 8080);
-  });
+    app.listen(process.env.PORT || 8080, () => {
+        console.log('Web/API Server is running on port', process.env.PORT || 8080);
+    });
 }
 
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from 'express'
-import { MessageEmbed } from 'discord.js'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express from 'express';
+import { MessageEmbed } from 'discord.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const router = express.Router()
+const router = express.Router();
 function generateEmbed(title, description, color, arrFieldNames, arrFieldValues) {
     return new Promise((resolve) => {
         if (typeof (arrFieldNames) != 'undefined') {
@@ -24,57 +24,60 @@ function generateEmbed(title, description, color, arrFieldNames, arrFieldValues)
             resolve(response);
         }
     });
-};
+}
 
 
 export const startServer = client => {
-    const app = express()
+    const app = express();
     app.use(express.json());
     app.use(express.urlencoded({
         extended: true
     }));
-    app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')))
-    app.set('view engine', 'ejs')
+    app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')));
+    app.set('view engine', 'ejs');
 
 
     app.get('/', async (_, res) => {
-        const title = 'Home'
+        const title = 'Home';
+        var guildCount = await client.guilds.fetch();
+        guildCount = guildCount.size;
         res.render('pages/index', {
-            title: title
-        })
-    })
+            title: title,
+            guildCount: guildCount
+        });
+    });
     app.get('/api', async (_, res) => {
-        res.sendStatus(404)
-    })
+        res.sendStatus(404);
+    });
     app.post('/api', async (req, res) => {
-        const data = req.body
+        const data = req.body;
         // console.log(data)
-        const auth = data.auth
+        const auth = data.auth;
 
         if (process.env.AUTHORIZATION === auth) {
-            const user = data.userid
-            const title = data.title
-            const message = data.message
-            const color = data.color
-            const fieldTitles = data.fieldTitles
-            const fieldValues = data.fieldValues
-            const member = await client.users.fetch(user)
+            const user = data.userid;
+            const title = data.title;
+            const message = data.message;
+            const color = data.color;
+            const fieldTitles = data.fieldTitles;
+            const fieldValues = data.fieldValues;
+            const member = await client.users.fetch(user);
 
-            const response = await generateEmbed(title, message, color, fieldTitles, fieldValues)
+            const response = await generateEmbed(title, message, color, fieldTitles, fieldValues);
             member
                 .createDM()
                 .then((DMChannel) => {
                     DMChannel
                         .send({ embeds: [response] })
-                        .catch(err => console.log(err, `Error with ${member}, Could not send DM, member has messages from server member's disabled`))
-                })
-            res.sendStatus(200)
+                        .catch(err => console.log(err, `Error with ${member}, Could not send DM, member has messages from server member's disabled`));
+                });
+            res.sendStatus(200);
         } else {
-            res.sendStatus(403)
+            res.sendStatus(403);
         }
 
-    })
+    });
 
-    app.use("/", router)
-    return app
-}
+    app.use('/', router);
+    return app;
+};
